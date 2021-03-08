07ae695148298754e53347aa75bf6d2985b3729d
Jerry <79889208+348428118@users.noreply.github.com>
2021-03-08 12:01:05 +0800

    Add files via upload

---
+++ display.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void display(const char * str){
+   printf("This is: %s.", str);
+ 
+  return;	
+}
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+extern void display(const char * str);
+
+int main(int art1){
+   display("hello world!\n");	
+   return 1;
+}
